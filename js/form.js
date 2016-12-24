2eecd9355f7f189f01db8f247297ae62e61c1ba4
Roman Khoronzhak <romix.price.3xd@gmail.com>
2016-12-24 00:06:27 +0000

    Include_form

---
+++ js/form.js
@@ -0,0 +1,21 @@
+/* global $ */
+$(function() {
+    $('.submit').click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/romanromchyk@hotmail.com",
+            method: "POST",
+            data: {
+                firstname: $('#firstname').val(),
+                lastname: $('#lastname').val(),
+                username: $('#username').val(),
+                password: $('#password').val(),
+                confpassword: $('#confpassword').val(),
+                email: $('#email').val()
+            },
+            dataType: "json"
+        }).done(function() {
+            $('form').html('<h1>Done!For return to "Home" click on logo :)</h1>');
+        });
+    });
+});
